99e144d59a4d7280a48550178071510ed4a68dbe
Gabriel Santerre <gabrielsanterre@gmail.com>
2013-09-06 20:56:48 -0400

    rien

--- www/app/views/search-header.blade.php
+++ www/app/views/search-header.blade.php
@@ -2,7 +2,7 @@
 	<div class="search-field-wrapper clearfix">
 		{{Form::open(array('url' => 'search'));}}
 		{{Form::text('','Trouvez étudiant, projet et plus',array('class' => 'search-field'));}}
-		<input type="image" class="search-btn" src="img/search-nav-btn.png" alt="Submit">
+		<input type="image" class="search-btn" src="img/search-nav-btn.png" alt="Submit" />
 		{{Form::close();}}
 	</div>
 </div>
