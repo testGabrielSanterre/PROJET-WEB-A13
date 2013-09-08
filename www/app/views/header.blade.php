297c7b96f066457993f7ddd93b509637a8290e5a
Gabriel Santerre <gabrielsanterre@gmail.com>
2013-09-08 18:54:30 -0400

    ajoute du side-nav-header

--- www/app/views/header.blade.php
+++ www/app/views/header.blade.php
@@ -1,8 +1,5 @@
 <header>
-	<div class="side-nav-header left clearfix">
-		<img class="side-nav-btn" src="img/side-nav-btn.png">
-		<h1 class="librexpo-title Open-Sans-italic-300">LIBRE<span class="Open-Sans-normal-700">XPO</span></h1>
-	</div>
+	@include('side-nav-header')
 	@include('nav-header')
 	@include('search-header')
 </header>
---
+++ www/app/views/side-nav-header.blade.php
@@ -0,0 +1,4 @@
+<div class="side-nav-header left clearfix">
+	<img class="side-nav-btn" src="img/side-nav-btn.png">
+	<h1 class="no-pm"><a class="librexpo-title Open-Sans-italic-300" href="#">LIBRE<span class="Open-Sans-normal-700">XPO</span></a></h1>
+</div>
