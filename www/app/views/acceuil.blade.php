8e294fe44569084043d55d3b23f1aaef491cf9b9
davidpatenaude <david.patenaudev4si@gmail.com>
2013-09-09 11:33:30 -0400

    Test

--- www/app/views/acceuil.blade.php
+++ www/app/views/acceuil.blade.php
@@ -4,7 +4,7 @@
 		<h1>Acceuil</h1>
 		@include('slider-accueil')
 		<p>
-			Un beau paragraphe de test pour tester je sais pas trop quoi mais bon ça devrait être assez long dans pas long comme paragraphe fack j'vais continuer d'écrire n'importe quoi quelque temps parce qu'il ne reste pas grand temps .. okay j'suis tanné fack c fini!
+			Un beau paragraphe de test pour tester je sais pas trop quoi mais bon ça devrait être assez long dans pas long comme paragraphe fack j'vais continuer d'écrire n'importe quoi quelque temps parce qu'il ne reste pas grand temps .. okay j'suis tanné fack c fini! OU pas !!!!!
 		</p>
 	@parent
 @stop
