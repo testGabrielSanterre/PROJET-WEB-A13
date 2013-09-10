eeeed5040f7910f3e980ee80fb2f8e869707276f
Gabriel Santerre <gabrielsanterre@gmail.com>
2013-09-10 18:23:36 -0400

    ajout d'un login

--- www/app/views/side-nav.blade.php
+++ www/app/views/side-nav.blade.php
@@ -1,3 +1,7 @@
-<div class="side-nav-wrapper">
-
+<div class="side-nav-wrapper unselectable">
+	{{Form::open(array('url' => 'login', 'class' => 'side-nav-login'));}}
+	{{Form::text('username','Nom d\'utilisateur');}}
+	{{Form::text('password','Mot de passe', array('class' => 'change-to-password'));}}
+	{{Form::submit('Connection');}}
+	{{Form::close();}}
 </div>
